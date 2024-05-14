7ccdca911a26131fd42920484769e3713df04538
13prashant <pacific1392@gmail.com>
2024-05-14 16:57:38 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,56 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [uploadingImage, setUploadingImage] = useState(false);
+
+  const onCropSubmit = async () => {
+    setUploadingImage(true); // Show loading indicator
+
+    try {
+      const img = await fetch("https://picsum.photos/200");
+      const croppedImageBlob = await img.blob();
+
+      // const croppedImageBlob = await getCroppedImg(image, croppedAreaPixels);
+      const fileExt = "jpeg"; // Assuming cropped image is in JPEG format
+      const photo = `profile-img-${"params?.id"}.${fileExt}`;
+      const croppedFile = new File([croppedImageBlob], photo, {
+        type: "image/jpeg",
+      });
+
+      const formData = new FormData();
+      formData.append("image", croppedFile);
+      formData.append("file-name", photo);
+      formData.append("folder", "profile"); // Assuming you're organizing uploaded images into folders
+
+      const response = await fetch("/api/upload-profile-pic", {
+        method: "POST",
+        body: formData,
+      });
+      if (!response.ok) {
+        throw new Error("Failed to upload cropped image.");
+      }
+      const result = await response.json();
+
+      const newImageUrl = `${
+        result.imageUrl
+      }?timestamp=${new Date().getTime()}`;
+      await updateProfilePicture(newImageUrl);
+      setImagePreview(newImageUrl); // Update image preview with the new image URL
+
+      setShowCropper(false); // Hide cropper
+    } catch (error) {
+      console.error("Error uploading cropped image:", error);
+      alert(error.message);
+    } finally {
+      setUploadingImage(false); // Hide loading indicator
+    }
+  };
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <button onClick={onCropSubmit}>Submit</button>
     </main>
   );
 }
